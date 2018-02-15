21a471ac84697ab3c5eeb52fa9338a8d184671e5
Nikita <Neeecky27@gmail.com>
2018-02-15 21:03:44 +0300

    добавил фектори для главной страницы

--- src/test/java/pochta/pages/MainPage.java
+++ src/test/java/pochta/pages/MainPage.java
@@ -1,4 +1,14 @@
 package pochta.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class MainPage {
+
+    public WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
 }
